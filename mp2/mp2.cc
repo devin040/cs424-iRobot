12f67197c540ea4f385f144442df2fbe451eb189
devin040 <devin.tark@gmail.com>
2019-10-23 16:41:41 -0500

    test motion code

--- mp2/mp2.cc
+++ mp2/mp2.cc
@@ -17,13 +17,13 @@ int main ()
   char serial_loc[] = "/dev/ttyUSB0";
 
   try {
-    raspicam::RaspiCam_Cv Camera;
-    cv::Mat rgb_image, bgr_image;
-    if (!Camera.open()) {
-      cerr << "Error opening the camera" << endl;
-      return -1;
-    }
-    cout << "Opened Camera" << endl;
+    //raspicam::RaspiCam_Cv Camera;
+    //cv::Mat rgb_image, bgr_image;
+    //if (!Camera.open()) {
+    //  cerr << "Error opening the camera" << endl;
+    //  return -1;
+    //}
+    //cout << "Opened Camera" << endl;
     SerialStream stream (serial_loc, LibSerial::SerialStreamBuf::BAUD_57600);
     cout << "Opened Serial Stream to" << serial_loc << endl;
     this_thread::sleep_for(chrono::milliseconds(1000));
@@ -46,8 +46,51 @@ int main ()
     song.push_back(Create::note_t(100, 8));
     song.push_back(Create::note_t(90, 8));
     robot.sendSongComand(1,song);
-
     robot.sendStreamCommand(robot);
+
+    while(!robot.advanceButton()){
+      
+      int speed = 200;
+      robot.sendDriveCommand (speed, Create::DRIVE_STRAIGHT);
+      if (robot.bumpLeft () || robot.bumpRight ()) {
+              speed = 0;
+              robot.sendDriveCommand(speed, Create::DRIVE_STRAIGHT);
+              this_thread::sleep_for(chrono::milliseconds(15));
+              speed = -152;
+              robot.sendDriveCommand(speed, Create::DRIVE_STRAIGHT);
+              this_thread::sleep_for(chrono::milliseconds(1000));
+              speed = 0;
+              robot.sendDriveCommand(speed, Create::DRIVE_STRAIGHT);
+              short maxWallSignal = 0;
+              bool localMaxFound = false;
+              short wallSignal = -1;
+              bool rotateRight = true;
+              bool rotateLeft = false;
+              bool sendRightCommand = true;
+              this_thread::sleep_for(chrono::milliseconds(200));
+
+              speed = 50;
+              robot.sendDriveCommand(speed, Create::DRIVE_INPLACE_COUNTERCLOCKWISE);
+              std::chrono::time_point start = std::chrono::steady_clock::now();
+              while (std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - start).count() < 3000){
+                  wallSignal = robot.wallSignal();
+                  if (wallSignal > maxWallSignal ){
+                      maxWallSignal = wallSignal;
+                  }
+                  std::this_thread::sleep_for(chrono::milliseconds(20));
+              }
+              robot.sendDriveCommand(speed, Create::DRIVE_INPLACE_CLOCKWISE);
+              while (robot.wallSignal() < maxWallSignal){
+                  std::this_thread::sleep_for(chrono::milliseconds(20));
+              }
+              speed = 0;
+              robot.sendDriveCommand(speed, Create::DRIVE_STRAIGHT);
+              std::this_thread::sleep_for(chrono::milliseconds(200));
+              speed = 100;
+              robot.sendDriveCommand(speed, Create::DRIVE_STRAIGHT);         
+      }
+
+    }
 
 
 }
--- mp2/robotmotion.cc
+++ mp2/robotmotion.cc
@@ -8,7 +8,7 @@
 
 using namespace iRobot;
 
-void robotMotion(Create& robot, pthread_mutex_t& robomutex){
+void robotMotion(Create& robot, pthread_mutex_t& robomutex, bool end){
 
     int speed = 200;
     robot.sendDriveCommand (speed, Create::DRIVE_STRAIGHT);
@@ -19,27 +19,38 @@ void robotMotion(Create& robot, pthread_mutex_t& robomutex){
             cout << "Bump : " << count++ << endl;
             speed = -152;
             robot.sendDriveCommand(speed, Create::DRIVE_STRAIGHT);
-            this_thread::sleep_for(chrono::milliseconds(1000));
-            short align = 0;
+            this_thread::sleep_for(chrono::milliseconds(1500));
+            speed = 0;
+            robot.sendDriveCommand(speed, Create::DRIVE_STRAIGHT);
+            short maxWallSignal = 0;
             bool localMaxFound = false;
-            rotateRight = true;
-            rotateLeft = false;
-            sendRightCommand = true;
-            while (!localMaxFound){
-                while (sendRightCommand){
-                    if (rotateRight){
-                        speed = 100;
-                        robot.sendDriveCommand(speed, Create::DRIVE_INPLACE_COUNTERCLOCKWISE);
-                        rotateRight = false;
-                    }
-                    
+            short wallSignal = -1;
+            bool rotateRight = true;
+            bool rotateLeft = false;
+            bool sendRightCommand = true;
+            this_thread::sleep_for(chrono::milliseconds(200));
 
+            speed = 50;
+            robot.sendDriveCommand(speed, Create::DRIVE_INPLACE_COUNTERCLOCKWISE);
+            std::chrono::time_point start = std::chrono::steady_clock::now();
+            while (std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - start).count() < 3000){
+                wallSignal = robot.wallSignal();
+                if (wallSignal > maxWallSignal ){
+                    maxWallSignal = wallSignal;
                 }
+                std::this_thread::sleep_for(chrono::milliseconds(20));
             }
+            robot.sendDriveCommand(speed, Create::DRIVE_INPLACE_CLOCKWISE);
+            while (robot.wallSignal() < maxWallSignal){
+                std::this_thread::sleep_for(chrono::milliseconds(20));
+            }
+            speed = 0;
+            robot.sendDriveCommand(speed, Create::DRIVE_STRAIGHT);
+            std::this_thread::sleep_for(chrono::milliseconds(200));
+            speed = 100;
+            robot.sendDriveCommand(speed, Create::DRIVE_STRAIGHT);
+                
     }
-
-
-
 
 }
 
