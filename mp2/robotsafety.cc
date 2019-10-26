e02a793d45df124c9867b734b40783d7c84349d4
Devin Tark <devinvt2@illinois.edu>
2019-10-26 18:29:48 -0500

    reverse sensors in safety

--- mp2/robotsafety.cc
+++ mp2/robotsafety.cc
@@ -14,8 +14,8 @@ void robotSafety(Create& robot, pthread_mutex_t& robomutex, bool& stop){
     while(!stop){
         pthread_mutex_lock(&robomutex);
         if (robot.wheeldropLeft() || robot.wheeldropRight() || robot.wheeldropCaster() ||
-                robot.cliffLeftSignal() >= 10 || robot.cliffFrontLeftSignal() >= 10 ||
-                robot.cliffRightSignal() >= 10 || robot.cliffFrontRightSignal() >= 10 ||
+                robot.cliffLeftSignal() < 10 || robot.cliffFrontLeftSignal() < 10 ||
+                robot.cliffRightSignal() < 10 || robot.cliffFrontRightSignal() < 10 ||
                 robot.leftWheelOvercurrent() || robot.rightWheelOvercurrent()) {
             //stop
             robot.sendDriveCommand(0, Create::DRIVE_STRAIGHT);
