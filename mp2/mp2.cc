43498cac0b8112db3fc3ba506b682803e55b5e44
Devin Tark <devinvt2@illinois.edu>
2019-10-26 16:13:13 -0500

    shorter backup and shorter avg

--- mp2/mp2.cc
+++ mp2/mp2.cc
@@ -64,7 +64,7 @@ int main ()
               robot.sendDriveCommand(0, Create::DRIVE_STRAIGHT);
               this_thread::sleep_for(chrono::milliseconds(15));
               robot.sendDriveCommand(-speed, Create::DRIVE_STRAIGHT);
-              this_thread::sleep_for(chrono::milliseconds(100));
+              this_thread::sleep_for(chrono::milliseconds(80));
               robot.sendDriveCommand(0, Create::DRIVE_STRAIGHT);
               this_thread::sleep_for(chrono::milliseconds(200));
               
@@ -112,7 +112,7 @@ int main ()
       wallSum += robot.wallSignal();
       cout << "Lost wall  Wall sum: " << wallSum << endl;
       wallCount++;
-      if (wallCount == 7){
+      if (wallCount == 6){
           wallAvg = wallSum / wallCount;
           wallCount = 0;
           wallSum = 0;
