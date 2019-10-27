e3dd6aea62a129b41645ec515fbea38c28ffec08
Devin Tark <devinvt2@illinois.edu>
2019-10-26 19:25:33 -0500

    locks2

--- mp2/robotsafety.cc
+++ mp2/robotsafety.cc
@@ -13,7 +13,7 @@ using namespace std;
 void robotSafety(Create& robot, pthread_mutex_t* robomutex, bool& stop){
     cout << "In safety thread" << endl;
     while(!stop){
-        pthread_mutex_lock(robomutex);
+        //pthread_mutex_lock(robomutex);
         if (robot.wheeldropLeft() || robot.wheeldropRight() || robot.wheeldropCaster() ||
                 robot.cliffLeftSignal() < 10 || robot.cliffFrontLeftSignal() < 10 ||
                 robot.cliffRightSignal() < 10 || robot.cliffFrontRightSignal() < 10 ||
@@ -32,7 +32,7 @@ void robotSafety(Create& robot, pthread_mutex_t* robomutex, bool& stop){
             //robot.sendDriveCommand(200, Create::DRIVE_STRAIGHT);
             
         }
-        pthread_mutex_unlock(robomutex);
+        //pthread_mutex_unlock(robomutex);
         this_thread::sleep_for(chrono::milliseconds(1100));
     } 
 }
