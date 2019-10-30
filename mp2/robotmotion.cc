0808d1ad9e305f189537d21c112db0f50b494522
Devin Tark <devinvt2@illinois.edu>
2019-10-29 21:00:54 -0500

    dont record short distances

--- mp2/robotmotion.cc
+++ mp2/robotmotion.cc
@@ -116,6 +116,9 @@ void robotMotion(Create& robot, pthread_mutex_t* robomutex, bool& end){
             distclock1 = std::chrono::steady_clock::now();
             int travTime = std::chrono::duration_cast<std::chrono::milliseconds>(distclock1-distclock0).count();
             float distance = ((float) travTime/ 1000.0 ) * (float) speed;
+            if (distance < 400){
+                break;
+            }
             distances.push_back(distance / 4 );
             angles.push_back(1.5707);
             cout << "Wrote a Write Turn distance: " << distance << endl;
