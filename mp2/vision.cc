8243a37948d6b1e405bdc3931530bdc7bd45f103
Hayne <hikim2@illinois.edu>
2019-11-02 20:10:07 -0500

    import

--- mp2/vision.cc
+++ mp2/vision.cc
@@ -2,6 +2,8 @@
 #include <dirent.h>
 #include <raspicam/raspicam_cv.h>
 #include "irobot-create.hh"
+#include <pthread>
+#include <thread>
 
 using namespace iRobot;
 using namespace std;
@@ -11,7 +13,6 @@ void robotCamera(Create&, raspicam::RaspiCam_Cv, pthread_mutex_t *, vector<Mat>,
 
 void processImages(vector<Mat> images) {
     int counterimage = 0;
-    Mat bgr_image;
     RobotIdentification test;
     for (int i = 0; i < images.size(); i++) {
         test.runIdentify(images[i]);
@@ -21,10 +22,11 @@ void processImages(vector<Mat> images) {
 void robotCamera(Create& robot, raspicam::RaspiCam_Cv Camera, pthread_mutex_t *stream_mutex, vector<Mat> images, bool& end) {
     while (!end){
         pthread_mutex_lock(stream_mutex);
-        speed = 50;
+        int speed = 50;
         robot.sendDriveCommand(speed, Create::DRIVE_STRAIGHT);
         pthread_mutex_unlock(stream_mutex);
         this_thread::sleep_for(std::chrono::milliseconds(2000));
+        Mat bgr_image;
         Camera.grab();
         Camera.retrieve (bgr_image);
         images.push_back(bgr_image);
