d442c19e2e601876ceac7862b9fda7f862238240
nidiaib2 <nidiaib2@illinois.edu>
2019-11-02 19:37:55 -0500

    edits to vision

--- mp2/vision.cc
+++ mp2/vision.cc
@@ -13,37 +13,32 @@ using std::endl;
 using std::string;
 using std::vector;
 int counterimage = 0;
-vector<Mat> images
-int intdex= 0
+vector<Mat> images;
+int index= 0;
+Mat bgr_image;
 
 
-try {
-  raspicam::RaspiCam_Cv Camera;
-  cv::Mat rgb_image, bgr_image;
-  if (!Camera.open()) {
-    cerr << "Error opening the camera" << endl;
-    return -1;
-}
 
-bool vision::vision ()
+void vision (Create& robot, pthread_mutex_t* stream_mutex, bool& end)
 {
 
-  pthread_mutex_lock(stream_mutex); /// NOT SURE ABOUT ARGUMENT
-  speed = 50;
-  robot.sendDriveCommand(speed, Create::DRIVE_STRAIGHT);
-  this_thread::sleep_for(chrono::milliseconds(100));
-  Camera.grab();
-  Camera.retrieve (bgr_image);
+  while (!end && counterimage < 10){
+    pthread_mutex_lock(stream_mutex);
+    speed = 50;
+    robot.sendDriveCommand(speed, Create::DRIVE_STRAIGHT);
+    this_thread::sleep_for(chrono::milliseconds(100));
+    Camera.grab();
+    Camera.retrieve (bgr_image);
 
-  //concatenating the name of the image with the number to do not replace files
-  std::string imagename = "irobot_image " + std::to_string(counterimage)
+    //concatenating the name of the image with the number to do not replace files
 
-  camera.imwrite(imagename, bgr_image)
-  images.push_back(img_scene)
 
-  pthread_mutex_unlock(stream_mutex); //NOT SURE ABOUT ARGUMENT
+    images.push_back(bgr_image);
 
-  counterimage= 1+ counterimage
+    pthread_mutex_unlock(stream_mutex); //NOT SURE ABOUT ARGUMENT
+    this_thread::sleep_for(std::chrono::milliseconds(1000));
+    counterimage++;
+  }
 }
 
 
