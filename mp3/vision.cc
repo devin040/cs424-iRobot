e53fa3a22ade6541796f19714e25c22af791b556
Hayne <hikim2@illinois.edu>
2019-11-18 14:38:17 -0600

    weapon added

--- mp3/vision.cc
+++ mp3/vision.cc
@@ -18,6 +18,7 @@ void processImages(vector<Mat> images) {
 pthread_mutex_t image_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void robotImage(Create& robot, pthread_mutex_t *stream_mutex, vector<Mat>& images, bool& end) {
+    bool usedWeapon = false;
     while (!end) {
         this_thread::sleep_for(std::chrono::milliseconds(3000));
         if (images.size() > 0) {
@@ -26,7 +27,13 @@ void robotImage(Create& robot, pthread_mutex_t *stream_mutex, vector<Mat>& image
             pthread_mutex_unlock(&image_mutex);
             if (test.runIdentify(image)) {
                 cout << "found magic lamp!" << endl;
-
+                if (!usedWeapon) {
+                    pthread_mutex_lock(&stream_mutex);
+                    robot.sendLedCommand(Create::LED_PLAY, Create::LED_COLOR_RED, Create::LED_INTENSITY_FULL);
+                    this_thread::sleep_for(chrono::milliseconds(2000));
+                    robot.sendLedCommand(Create::LED_PLAY, 0, 0);
+                    pthread_mutex_unlock(&stream_mutex);
+                }
             }
             cout << "Processing picture taken..." << endl;
         }
