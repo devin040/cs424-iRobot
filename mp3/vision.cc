e8ca6ea34c058c6c80b6a2d1c91e35a349e0c79a
Devin Tark <devinvt2@illinois.edu>
2019-11-19 22:21:00 -0600

    timers fix

--- mp3/vision.cc
+++ mp3/vision.cc
@@ -16,6 +16,7 @@ void processImages(vector<Mat> images) {
 
 void robotImage(Create& robot, pthread_mutex_t *stream_mutex, pthread_mutex_t *image_mutex, vector<Mat>& images, bool& end) {
     bool usedWeapon = false;
+    int procimagecount = 0;
     RobotIdentification test;
     vector<Mat> rest_images;
     while (!end) {
@@ -60,6 +61,7 @@ void robotImage(Create& robot, pthread_mutex_t *stream_mutex, pthread_mutex_t *i
                     test.runIdentify(image);
                 }
             }
+            cout << "CURRENTLY PROCESSING TAKEN IMAGE: " << procimagecount << endl;
         }
 
     }
