0ec240965b4d6a8f84bed0cdf8ffd2ebea8dddf1
Devin Tark <devinvt2@illinois.edu>
2019-12-05 16:01:55 -0600

    motion fix

--- mp4/robotmotion.cc
+++ mp4/robotmotion.cc
@@ -71,177 +71,51 @@ void robotMotion(Create& robot, pthread_mutex_t* robomutex, pthread_mutex_t* cam
             speed = TRAVELSPEED;
             robot.sendDriveCommand(TRAVELSPEED, Create::DRIVE_STRAIGHT);
             this_thread::sleep_for(chrono::milliseconds(50));
-        }
+        } else {
  
-        cout << "Continous wall sensor: " << robot.wallSignal() << endl;
-        this_thread::sleep_for(chrono::milliseconds(15));
-        short wallsig = robot.wallSignal();
-        short prevWall = wallsig;
-        while(true){
-            robot.sendDriveCommand(TRAVELSPEED, Create::DRIVE_STRAIGHT);
-            short desiredWallSigLow = 80;
-            short desiredWallSigHigh = 100;
-            prevWall = wallsig;
-            wallsig = robot.wallSignal();
-
-            if (wallsig < 30){
-                robot.sendDriveCommand(0, Create::DRIVE_STRAIGHT);
-                TSLEEP(200);
-                cout << "WOAH: WAll sig: " << wallsig << endl;
-                findMax(std::ref(robot));
-                cout << "OKAY: Wall sig: " << robot.wallSignal() << endl;
+            cout << "Continous wall sensor: " << robot.wallSignal() << endl;
+            this_thread::sleep_for(chrono::milliseconds(15));
+            short wallsig = robot.wallSignal();
+            short prevWall = wallsig;
+            while(true){
                 robot.sendDriveCommand(TRAVELSPEED, Create::DRIVE_STRAIGHT);
-                TSLEEP(50);
-            } else {
-                if (wallsig < desiredWallSigLow){
-                short radius = -100 + wallsig / (float) desiredWallSigLow * -750;
-                robot.sendDriveCommand(TRAVELSPEED, radius);
+                short desiredWallSigLow = 80;
+                short desiredWallSigHigh = 100;
                 prevWall = wallsig;
-                cout << "Radius : " << radius << endl;
-                TSLEEP(15);
-                }
-                if ((wallsig) > desiredWallSigHigh){
-                    short radius = 1000 - (wallsig - desiredWallSigHigh)/ (float) desiredWallSigHigh * 750;
+                wallsig = robot.wallSignal();
+
+                if (wallsig < 30){
+                    robot.sendDriveCommand(0, Create::DRIVE_STRAIGHT);
+                    TSLEEP(200);
+                    cout << "WOAH: WAll sig: " << wallsig << endl;
+                    findMax(std::ref(robot));
+                    cout << "OKAY: Wall sig: " << robot.wallSignal() << endl;
+                    robot.sendDriveCommand(TRAVELSPEED, Create::DRIVE_STRAIGHT);
+                    TSLEEP(50);
+                } else {
+                    if (wallsig < desiredWallSigLow){
+                    short radius = -100 + wallsig / (float) desiredWallSigLow * -750;
                     robot.sendDriveCommand(TRAVELSPEED, radius);
                     prevWall = wallsig;
                     cout << "Radius : " << radius << endl;
                     TSLEEP(15);
+                    }
+                    if ((wallsig) > desiredWallSigHigh){
+                        short radius = 1000 - (wallsig - desiredWallSigHigh)/ (float) desiredWallSigHigh * 750;
+                        robot.sendDriveCommand(TRAVELSPEED, radius);
+                        prevWall = wallsig;
+                        cout << "Radius : " << radius << endl;
+                        TSLEEP(15);
 
+                    }
                 }
+
+                cout << "Wall Sig: " << wallsig << endl;
+                TSLEEP(500);
             }
-
-            
-            // if (wallsig < 20){
-            //     robot.sendDriveCommand(0, Create::DRIVE_STRAIGHT);
-            //     this_thread::sleep_for(chrono::milliseconds(50));
-
-            //     short maxWallSignal = 0;
-            //     short wallSignal = -1;
-
-            //     speed = 100;
-            //     robot.sendDriveCommand(speed, Create::DRIVE_INPLACE_COUNTERCLOCKWISE);
-            //     std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
-                
-            //     while (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start).count() < 2200){
-            //         wallSignal = robot.wallSignal();
-            //         //cout << "Wall signal: " << wallSignal << endl;
-            //         if (wallSignal >= maxWallSignal ){
-            //             maxWallSignal = wallSignal;
-
-            //         }
-            //         this_thread::sleep_for(chrono::milliseconds(15));
-            //     }
-
-            //     cout << "MAX WALL SIGNAL: " << maxWallSignal << endl;
-            //     robot.sendDriveCommand(speed, Create::DRIVE_INPLACE_CLOCKWISE);
-
-
-            //     while ((wallSignal = robot.wallSignal()) < (maxWallSignal - 30)){
-
-            //         std::this_thread::sleep_for(chrono::milliseconds(15));
-            //     }
-
-            //     robot.sendDriveCommand(0, Create::DRIVE_STRAIGHT);
-            //     this_thread::sleep_for(chrono::milliseconds(50));
-            //     robot.sendDriveCommand(speed, Create::DRIVE_INPLACE_COUNTERCLOCKWISE);
-            //     this_thread::sleep_for(chrono::milliseconds(200));
-            //     robot.sendDriveCommand(0, Create::DRIVE_STRAIGHT);
-            //     this_thread::sleep_for(chrono::milliseconds(50));
-            //     speed = TRAVELSPEED;
-            //     robot.sendDriveCommand(TRAVELSPEED, Create::DRIVE_STRAIGHT);
-            //     this_thread::sleep_for(chrono::milliseconds(50));
-            // }
-            
-
-           
-
-
-            cout << "Wall Sig: " << wallsig << endl;
-            TSLEEP(500);
-
-        }
-/**
-        if (enteredMaze && (robot.wallSignal() < 3) && numTurns == 0 && numAdjust < 2){
- 
-            robot.sendDriveCommand(TRAVELSPEED, Create::DRIVE_INPLACE_CLOCKWISE);
-            this_thread::sleep_for(chrono::milliseconds(100));
-            robot.sendDriveCommand(0, Create::DRIVE_STRAIGHT);
-            this_thread::sleep_for(chrono::milliseconds(15));
-            robot.sendDriveCommand(TRAVELSPEED, Create::DRIVE_STRAIGHT);
-            this_thread::sleep_for(chrono::milliseconds(100));
-
-        }
-        this_thread::sleep_for(chrono::milliseconds(15));
-        if (enteredMaze && (robot.wallSignal() > 90) ){
-            robot.sendDriveCommand(200, Create::DRIVE_INPLACE_COUNTERCLOCKWISE);
-            this_thread::sleep_for(chrono::milliseconds(50));
-            robot.sendDriveCommand(SPEED, Create::DRIVE_STRAIGHT);
-            this_thread::sleep_for(chrono::milliseconds(50));
-
         }
 
-        robot.sendDriveCommand(TRAVELSPEED, Create::DRIVE_STRAIGHT);
-        TSLEEP(2000);
-        robot.sendDriveCommand(TRAVELSPEED, -1999);
-        cout << "Turn at -1999" << endl; 
-        TSLEEP(2000);
-        robot.sendDriveCommand(TRAVELSPEED, Create::DRIVE_STRAIGHT);
-        TSLEEP(2000);
-        robot.sendDriveCommand(TRAVELSPEED, 1999);
-        cout << "Turn at 1999" << endl; 
-        TSLEEP(2000);
-        robot.sendDriveCommand(TRAVELSPEED, Create::DRIVE_STRAIGHT);
-        TSLEEP(2000);
-        robot.sendDriveCommand(TRAVELSPEED, -1000);
-        cout << "Turn at -1000" << endl; 
-        TSLEEP(2000);
-        robot.sendDriveCommand(TRAVELSPEED, Create::DRIVE_STRAIGHT);
-        TSLEEP(2000);
-        robot.sendDriveCommand(TRAVELSPEED, 1000);
-        cout << "Turn at 1000" << endl; 
-        TSLEEP(2000);
-        robot.sendDriveCommand(TRAVELSPEED, Create::DRIVE_STRAIGHT);
-        TSLEEP(2000);
-        robot.sendDriveCommand(TRAVELSPEED, -500);
-        cout << "Turn at -500" << endl; 
-        TSLEEP(2000);
-        robot.sendDriveCommand(TRAVELSPEED, Create::DRIVE_STRAIGHT);
-        TSLEEP(2000);
-        robot.sendDriveCommand(TRAVELSPEED, 500);
-        cout << "Turn at 500" << endl; 
-        TSLEEP(2000);
-        robot.sendDriveCommand(TRAVELSPEED, Create::DRIVE_STRAIGHT);
-        TSLEEP(2000);
-        robot.sendDriveCommand(TRAVELSPEED, -250);
-        cout << "Turn at -250" << endl; 
-        TSLEEP(2000);
-        robot.sendDriveCommand(TRAVELSPEED, Create::DRIVE_STRAIGHT);
-        TSLEEP(2000);
-        robot.sendDriveCommand(TRAVELSPEED, 250);
-        cout << "Turn at 250" << endl; 
-        TSLEEP(2000);
-        robot.sendDriveCommand(TRAVELSPEED, Create::DRIVE_STRAIGHT);
-        TSLEEP(2000);
-        robot.sendDriveCommand(TRAVELSPEED, -100);
-        cout << "Turn at -100" << endl; 
-        TSLEEP(2000);
-        robot.sendDriveCommand(TRAVELSPEED, Create::DRIVE_STRAIGHT);
-        TSLEEP(2000);
-        robot.sendDriveCommand(TRAVELSPEED, 100);
-        cout << "Turn at 100" << endl; 
-        TSLEEP(2000);
-        robot.sendDriveCommand(TRAVELSPEED, Create::DRIVE_STRAIGHT);
-        TSLEEP(2000);
-        robot.sendDriveCommand(TRAVELSPEED, -50);
-        cout << "Turn at -50" << endl; 
-        TSLEEP(2000);
-        robot.sendDriveCommand(TRAVELSPEED, Create::DRIVE_STRAIGHT);
-        TSLEEP(2000);
-        robot.sendDriveCommand(TRAVELSPEED, 50);
-        cout << "Turn at 50" << endl; 
-        TSLEEP(2000);
-        robot.sendDriveCommand(TRAVELSPEED, Create::DRIVE_STRAIGHT);
-        */
+
         cout << "Wall Signal : " << robot.wallSignal() << endl;
         
 
